70852f8f9d27c79ea827d222691f970425fe5642
Yen-Chin Lee <coldnew.tw@gmail.com>
2016-01-09 18:59:03 +0800

    fix sourcecode indent

--- guile/hello/hello-core.c
+++ guile/hello/hello-core.c
@@ -50,41 +50,41 @@ Fcall_hello(emacs_env *env, ptrdiff_t nargs, emacs_value args[], void *data)
 // Lisp utilities for easier readability (simple wrappers).
 
 // Provide FEATURE to Emacs.
-        static void
-                provide (emacs_env *env, const char *feature)
-        {
-                emacs_value Qfeat = env->intern (env, feature);
-                emacs_value Qprovide = env->intern (env, "provide");
-                emacs_value args[] = { Qfeat };
+static void
+provide (emacs_env *env, const char *feature)
+{
+        emacs_value Qfeat = env->intern (env, feature);
+        emacs_value Qprovide = env->intern (env, "provide");
+        emacs_value args[] = { Qfeat };
 
-                env->funcall (env, Qprovide, 1, args);
-        }
+        env->funcall (env, Qprovide, 1, args);
+}
 
 // Bind NAME to FUN.
-        static void
-                bind_function (emacs_env *env, const char *name, emacs_value Sfun)
-        {
-                emacs_value Qfset = env->intern (env, "fset");
-                emacs_value Qsym = env->intern (env, name);
-                emacs_value args[] = { Qsym, Sfun };
+static void
+bind_function (emacs_env *env, const char *name, emacs_value Sfun)
+{
+        emacs_value Qfset = env->intern (env, "fset");
+        emacs_value Qsym = env->intern (env, name);
+        emacs_value args[] = { Qsym, Sfun };
 
-                env->funcall (env, Qfset, 2, args);
-        }
+        env->funcall (env, Qfset, 2, args);
+}
 
 // Module init function.
-        int
-                emacs_module_init(struct emacs_runtime *ert)
-        {
-                emacs_env *env = ert->get_environment(ert);
+int
+emacs_module_init(struct emacs_runtime *ert)
+{
+        emacs_env *env = ert->get_environment(ert);
 
 #define DEFUN(lsym, csym, amin, amax, doc, data)                        \
-                bind_function (env, lsym,                               \
-                               env->make_function (env, amin, amax, csym, doc, data))
+        bind_function (env, lsym,                                       \
+                       env->make_function (env, amin, amax, csym, doc, data))
 
-                        DEFUN ("hello-c", Fcall_hello, 0, 0, "Return \"Hello Emacs\".", NULL);
+        DEFUN ("hello-c", Fcall_hello, 0, 0, "Return \"Hello Emacs\".", NULL);
 
 #undef DEFUN
 
-                        provide(env, "hello-core");
-                        return 0;
-        }
+        provide(env, "hello-core");
+        return 0;
+}
